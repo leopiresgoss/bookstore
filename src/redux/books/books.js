923df2302259166d35d1936a3cbc110c56e35132
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-28 11:00:32 -0600

    Add books reducer and its action creators

---
+++ src/redux/books/books.js
@@ -0,0 +1,46 @@
+let nextId = 0;
+
+// Actions
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+// Reducer
+export default function booksReducer(state = [], action) {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        action.book,
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default: return state;
+  }
+}
+
+// Action creators
+/*
+  book = {
+    id:
+    title:
+    author:
+  }
+*/
+export function addBookAction(book) {
+  nextId += 1;
+
+  return {
+    type: ADD_BOOK,
+    payload: {
+      ...book,
+      id: nextId,
+    },
+  };
+}
+
+export function removeBookAction(id) {
+  return {
+    type: REMOVE_BOOK,
+    id,
+  };
+}
