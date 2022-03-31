c295742aff30755d6a72cf54813ae5f911c818c3
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-31 15:08:24 -0600

    Improve book style and add progress circle script

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBookAction } from '../redux/books/books';
+import './circle.css';
 
 const Book = (props) => {
   const { bookItem } = props;
@@ -13,17 +14,46 @@ const Book = (props) => {
     dispatch(removeBookAction(id));
   };
 
+  // updating this number, it will also update the progress circle
+  const percentage = 0.64;
+
+  const progressCircleStyle = {
+    background: `
+    conic-gradient(
+      #0290ff ${percentage * 360}deg,
+      #e8e8e8 calc(${percentage * 360}deg
+    )
+    `,
+  };
+
   return (
     <li className="book">
       <div className="details">
         <p className="category">{bookItem.category}</p>
         <h3 className="title">{bookItem.title}</h3>
         <p className="author">{bookItem.author}</p>
+        <div className="buttons">
+          <button type="button" className="remove-btn" data-id={bookItem.item_id} onClick={removeBook}>
+            Remove
+          </button>
+        </div>
       </div>
-      <div className="buttons">
-        <button type="button" className="remove-btn" data-id={bookItem.item_id} onClick={removeBook}>
-          Remove
-        </button>
+      <div className="complete">
+        <div className="details">
+          <div className="wrapper" style={progressCircleStyle} />
+          <div>
+            <p className="percentage">
+              {Number(percentage) * 100}
+              %
+            </p>
+            <p className="completed">Completed</p>
+          </div>
+        </div>
+        <div className="progress">
+          <h6>CURRENT CHAPTER</h6>
+          <p>Chapter 1</p>
+          <button type="button" className="update-btn">UPDATE PROGRESS</button>
+        </div>
       </div>
     </li>
   );
